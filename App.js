2edfc8db493623ddf4c9c6e922c6bfcaa681031c
Siri <siribobbili410@gmail.com>
2025-01-04 10:45:56 +0530

    Practice 3

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
   import React from 'react';
-  import ReactDOM from "react-dom"
-const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"}, [React.createElement("h1",{}, "I am H1 Tag"), React.createElement("h2",{}, "I am H2 Tag")]), 
-    React.createElement("div",{id:"child2"}, [React.createElement("h3",{}, "I am H3 Tag"), React.createElement("h4",{}, "I am H4 Tag")])
-    ])
-   
+  import ReactDOM from "react-dom/client"
+const parent = <h1 id="heading">Hello World with JSX</h1>
+const Component =  () =>{
+  return (
+    <>
+    <h1>Hellow World with React</h1>
+    {parent}
+    </>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+root.render(<Component />);
